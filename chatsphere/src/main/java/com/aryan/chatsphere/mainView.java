f2437b1b14bfe4a713da26ed5536b53490a416bd
Aryan <119639573+Aryan-javamaster@users.noreply.github.com>
2023-03-26 11:29:51 -0500

    Update mainView.java

--- chatsphere/src/main/java/com/aryan/chatsphere/mainView.java
+++ chatsphere/src/main/java/com/aryan/chatsphere/mainView.java
@@ -11,74 +11,82 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
-
 @Route("/")
-
 public class mainView extends VerticalLayout {
     private String name;
     private Grid grid;
     private TextField textField;
+
+    // Constructor
     public mainView() throws SQLException {
-        //name feild
-       textField = new TextField();
+        // Name field
+        textField = new TextField();
         textField.setLabel("Name");
         textField.setRequiredIndicatorVisible(true);
         textField.setErrorMessage("This field is required");
 
-        //login button
-        Button login = new Button("Button");
-        VerticalLayout Layout = new VerticalLayout(textField,login);
+        // Login button
+        Button login = new Button("Login");
+        VerticalLayout Layout = new VerticalLayout(textField, login);
+       
         add(Layout);
+
+        // Login button click listener
         login.addClickListener(clickEvent -> {
             name = textField.getValue();
             try {
                 chatView();
-                Layout.remove(textField,login);
+                Layout.remove(textField, login);
             } catch (SQLException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         });
-
     }
 
+    // Chat view method
     public void chatView() throws SQLException {
-  
+        // Initialize grid
         grid = new Grid<>(chat.class, true);
         ArrayList<chat> chat = database.get();
         grid.setItems(chat);
+
+        // Chat and submit fields
         TextField chatfield = new TextField();
         chatfield.setLabel("Chat");
         TextField submit = new TextField();
-        submit.setLabel("Submit");
+        submit.setLabel("Chat");
         submit.setRequiredIndicatorVisible(true);
-        Button submitButton = new Button("Button");
-        add(grid,submit,submitButton);
+
+        // Submit button
+        Button submitButton = new Button("Send");
+        add(grid, submit, submitButton);
+
+        // Submit button click listener
         submitButton.addClickListener(clickEvent -> {
             name = textField.getValue();
             try {
-                database.commit(name, dateandtime.getTime(),  dateandtime.getDate(), submit.getValue());
+                // Commit chat to database
+                database.commit(name, dateandtime.getTime(), dateandtime.getDate(), submit.getValue());
                 submit.clear();
             } catch (SQLException e) {
-               
                 e.printStackTrace();
             }
         });
+
+        // Update grid periodically
         UI.getCurrent().setPollInterval(1500);
         UI.getCurrent().addPollListener(event -> {
             try{
                 gridUpdate();
-            }
-            catch (SQLException e){
+            } catch (SQLException e){
                 e.printStackTrace(); 
             }
         });
     }
+
+    // Update grid method
     public void gridUpdate() throws SQLException {
         ArrayList<chat> people = database.get();
         grid.setItems(people);
     }
-
 }
-    
-
